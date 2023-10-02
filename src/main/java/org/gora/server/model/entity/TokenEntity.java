040bcde50f52c9b9fbdc03a70fa2bf9668e3efcc
mosehun <ehaakdl@gmail.com>
2023-10-02 12:21:20 +0900

    refactor: clean code

--- src/main/java/org/gora/server/model/entity/TokenEntity.java
+++ src/main/java/org/gora/server/model/entity/TokenEntity.java
@@ -5,6 +5,8 @@ import java.util.Date;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -42,6 +44,7 @@ public class TokenEntity {
     @Column
     private String access;
     @Column
+    @Enumerated(EnumType.STRING)
     private eTokenUseDBType type;
 
     public void setEmailVerify(EmailVerifyEntity emailVerify) {
