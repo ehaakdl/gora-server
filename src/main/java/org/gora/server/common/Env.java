67c1a08f36f3eacab07da96e2481ad36c50506ad
mosehun <ehaakdl@gmail.com>
2023-10-08 18:01:40 +0900

    feature: dockerize

--- src/main/java/org/gora/server/common/Env.java
+++ src/main/java/org/gora/server/common/Env.java
@@ -5,8 +5,5 @@ import lombok.Getter;
 @Getter
 public class Env {
     public static String MAX_DEFAULT_QUE_SZ = "MAX_DEFAULT_QUE_SZ";
-    public static String UDP_CLIENT_PORT = "UDP_CLIENT_PORT";
-    public static String UDP_SERVER_PORT = "UDP_SERVER_PORT";
-    public static String TCP_SERVER_PORT = "TCP_SERVER_PORT";
     
 }
