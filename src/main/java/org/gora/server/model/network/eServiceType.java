e9ac7b7c6db547d1f748faaa334296af0fc7e5af
mosehun <ehaakdl@naver.com>
2023-12-07 18:25:13 +0900

    feature: 채팅 프로토콜버퍼 포멧 추가

--- src/main/java/org/gora/server/model/network/eServiceType.java
+++ src/main/java/org/gora/server/model/network/eServiceType.java
@@ -6,19 +6,19 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum eServiceType {
-    player_coordinate(1)
-    , test(2)
-    , health_check(3)
+    player_coordinate(1), test(2), health_check(3), chat_msg_from_client(4), chat_msg_from_server(5)
+
     ;
+
     private final int type;
 
-    public static eServiceType convert(int type){
+    public static eServiceType convert(int type) {
         for (eServiceType value : values()) {
-            if(value.getType() == type){
+            if (value.getType() == type) {
                 return value;
             }
         }
-        
+
         return null;
     }
 }
